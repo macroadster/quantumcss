4b7de4dd4ba9e88b3f132af8f6aa50b4c0e54223
Eric Yang <eyang@apache.org>
2026-03-01 10:10:02 -0800

    chore: bump version to 1.7.0 and update documentation

--- scripts/build.js
+++ scripts/build.js
@@ -80,7 +80,7 @@ class QuantumCSSBuilder {
 
     // Add banner
     const banner = `/*!
- * QuantumCSS + Starlight UI v1.6.2 - Advanced Utility-First CSS Framework
+ * QuantumCSS + Starlight UI v1.7.0 - Advanced Utility-First CSS Framework
  * https://github.com/macroadster/quantumcss
  * Copyright (c) 2026 Eric Yang
  * License: MIT
