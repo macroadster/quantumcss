3e0708ca192e8c40ac894404e6e5c8ea59790470
Eric Yang <eyang@apache.org>
2026-03-27 10:18:59 -0700

    Release v1.11.4: Navigation & UI Refinement

--- scripts/build.js
+++ scripts/build.js
@@ -48,7 +48,7 @@ class QuantumCSSBuilder {
     console.log('\n🚀 Building QuantumCSS...');
     
     let combinedCSS = '/*!\n' +
-      ' * QuantumCSS + Starlight UI v1.11.3 - Advanced Utility-First CSS Framework\n' +
+      ' * QuantumCSS + Starlight UI v1.11.4 - Advanced Utility-First CSS Framework\n' +
       ' * License: MIT\n' +
       ' */\n\n';
 
